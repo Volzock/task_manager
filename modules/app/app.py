30447af1a05c3cfebfc1454a3384e43fe5fb9039
Nikita Kozlov <nukfeed@yandex.ru>
2021-02-10 22:49:06 +0300

    add help method to app

--- modules/app/app.py
+++ modules/app/app.py
@@ -1,3 +1,5 @@
+import pickle
+
 from modules.task.task import Task
 from modules.task_group.task_group import TaskGroup
 
@@ -7,39 +9,51 @@ class Application:
     def __init__(self):
         self.tasks = []
         self.groups = []
+        self.history = []
 
     def start(self):
-        while True:
-            cmd = input("app> ").lower().strip().split()
-            if cmd[0] == "make":
-                if cmd[1] == "task":
-                    Task.add_task(self.tasks)
-                elif cmd[1] == "group":
-                    TaskGroup.add_group(self.groups)
-            elif cmd[0] == "list":
-                pass
-            elif cmd[0] == "edit":
-                pass
-            elif cmd[0] == "view":
-                pass
-            elif cmd[0] == "remind":
-                pass
-            elif cmd[0] == "remove":
-                pass
-            elif cmd[0] == "done":
-                pass
-            elif cmd[0] == "add":
-                pass
-            elif cmd[1] == "group":
-                pass
-            elif cmd[1] == "task":
-                pass
-            elif cmd[1] == "tasks":
-                pass
-            elif cmd[1] == "groups":
-                pass
-            elif cmd[1] == "help":
-                pass
+        try:
+            while True:
+                cmd = input("app: ").lower().strip().split()
+                if cmd[0] == "make":
+                    if cmd[1] == "task":
+                        Task.add_task(self.tasks)
+                    elif cmd[1] == "group":
+                        TaskGroup.add_group(self.groups)
+                elif cmd[0] == "list":
+                    if cmd[1] == "tasks":
+                        pass
+                    elif cmd[1] == "groups":
+                        pass
+                elif cmd[0] == "edit":
+                    pass
+                elif cmd[0] == "view":
+                    pass
+                elif cmd[0] == "remove":
+                    if cmd[1] == "task":
+                        self.remove_task(cmd[2])
+                    elif cmd[1] == "group":
+                        self.remove_group(cmd[2])
+                elif cmd[0] == "done":
+                    pass
+                elif cmd[0] == "add":
+                    pass
+                elif cmd[0] == "help":
+                    self.print_help()
+        except KeyboardInterrupt:
+            print("Bye bye")
+
+    def remove_task(self, name):
+        pass
+
+    def remove_group(self, name):
+        pass
+
+    def print_help(self):
+        with open("data/help.txt") as help_txt:
+            for string in help_txt:
+                print(string)
+        print()
 
     def __del__(self):
         pass
